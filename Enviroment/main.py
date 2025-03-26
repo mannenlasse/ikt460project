ccd44f3b1727a6a2f9e7af7a01faa991e677dfd4
mannenlasse <02ammar.ali@gmail.com>
2025-03-26 17:03:58 +0100

    init

--- Enviroment/main.py
+++ Enviroment/main.py
@@ -1,1 +1,17 @@
-print("ammar er gay. Yeet!")
+import numpy as np
+
+
+class Game: 
+
+    def __init__(self):
+        self.board_height = 6 
+        self.board_width = 7
+        self.winning_length = 4
+        self.Player1 = True  #Player 1 starts
+        self.Winner = None  # Winner status (starting with none)
+
+
+
+
+
+    print("ammar er gay. Yeet!")
