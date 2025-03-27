1696e4e8b53ef609d73ee1aa9900527570aeeb00
mannenlasse <02ammar.ali@gmail.com>
2025-03-27 18:18:22 +0100

    print board with numpy
    just prints out a numpy board which is supposed to be the connect four board.

--- Enviroment/main.py
+++ Enviroment/main.py
@@ -3,9 +3,12 @@ import numpy as np
 
 class Game: 
 
-    def __init__(self):
-        self.board_height = 6 
-        self.board_width = 7
+    def __init__(self, height, width):
+
+        self.board_height = height
+        self.board_width = width
+
+        self.board = np.zeros((self.board_height, self.board_width), dtype=int)
         self.winning_length = 4
         self.Player1 = True  #Player 1 starts
         self.Winner = None  # Winner status (starting with none)
@@ -14,4 +17,12 @@ class Game:
 
 
 
-    print("ammar er gay. Yeet!")
+    def SimulateGame(self):
+        print("ammar er gay. Yeet!")
+        print(self.board)
+
+
+
+game = Game(6,7)
+
+game.SimulateGame()
