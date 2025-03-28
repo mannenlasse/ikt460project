2ea37d336d5754100b3d7584ab95642bd4816d95
mannenlasse <02ammar.ali@gmail.com>
2025-03-28 03:44:54 +0100

    random moves
    so right now its 1v1 and its random moves until the board is filled

--- Enviroment/main.py
+++ Enviroment/main.py
@@ -1,28 +1,64 @@
 import numpy as np
-
+import random
 
 class Game: 
 
     def __init__(self, height, width):
-
         self.board_height = height
         self.board_width = width
 
         self.board = np.zeros((self.board_height, self.board_width), dtype=int)
         self.winning_length = 4
-        self.Player1 = True  #Player 1 starts
-        self.Winner = None  # Winner status (starting with none)
+        self.current_player = 1  
 
 
 
+    def make_move(self, col):
+        for row in reversed(range(self.board_height)):
+            if self.board[row, col] == 0:
+                self.board[row, col] = self.current_player
+                return True  # <--- ADD THIS!
+        
+        print("make_move: no available moves")
+        return False
 
 
-    def SimulateGame(self):
-        print("ammar er gay. Yeet!")
-        print(self.board)
+
+    def random_available_column(self):
+        available_columns = [col for col in range(self.board_width) if self.board[0, col] == 0]
+        if not available_columns:
+            print("random_available_column: no cells available")
+            return None
+        
+        random_move = random.choice(available_columns)
+
+        print(f"random_available_column: chose: {random_move} as a random column")
+
+        
+        return random_move
+
+    def print_board(self):
+        print(np.flip(self.board))
 
 
+    def simulate_game(self):
+        print("SimulateGame: started\n")
 
-game = Game(6,7)
+        while True:
+            col = self.random_available_column()
+            if col is None:
+                print("Board is full\n")
+                break
 
-game.SimulateGame()
+            self.make_move(col)
+            print(f"Player {self.current_player} played in column {col}")
+            self.print_board()
+            print("")
+
+            # Switch player (for now, between 1 and 2)
+            self.current_player = 2 if self.current_player == 1 else 1
+
+
+game = Game(6,9)
+
+game.simulate_game()
